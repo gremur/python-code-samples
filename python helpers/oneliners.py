fcbe0cd1d7eab29d16b49cae36028b1ba0a36482
Grey Murav <65895033+greymur@users.noreply.github.com>
2022-03-28 18:36:57 +0300

    added split list on chunks by n qty of items one-liner

--- python helpers/oneliners.py
+++ python helpers/oneliners.py
@@ -1,5 +1,14 @@
 # flatten list - convert list of lists or tuples like [(1, 2)] to flat list of items like [1, 2]
 lst = [(1, 2)]
-result = [item for subitem in lst for item in subitem]
+result1 = [item for subitem in lst for item in subitem]
+
+
+# split list on chunks where
+# n - chunk size
+# lst - list of items
+lst = [1, 2, 2, 4, 5, 6]
+n = 2
+result2 = [lst[i:i + n] for i in range(0, len(lst), n)]
+# print(result2) returns [[1, 2], [2, 4], [5, 6]]
 
 
